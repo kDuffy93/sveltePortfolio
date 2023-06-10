e56da605cb7de0ffbe2d821c207a71ab309bf3f7
Kyle Duffy <kyleduffy83@gmail.com>
2023-06-10 05:59:30 -0400

    about page layout update

--- src/components/pages/about/about.svelte
+++ src/components/pages/about/about.svelte
@@ -5,26 +5,32 @@
 	<div>
 		<img src="Assets/20221022_043327.jpg" />
 	</div>
+
+
+	<p>
+		With over 13 years of experience in software development, I have cultivated a passion for
+		programming and a deep appreciation for the art of problem-solving.
+	</p>
+	<p>
+		I discovered my love for programming during high school when I was introduced to Java. The
+		elegance and flexibility of the language captivated me, igniting a curiosity to explore its
+		endless possibilities. From that point on, I delved deeper into the world of coding,
+		continuously expanding my knowledge and skills.
+	</p>
+	<p>
+		I am always eager to take on new challenges, collaborate with talented individuals, and stay up
+		to date with the latest advancements in technology. My commitment to continuous learning and my
+		passion for programming drive me to deliver high-quality solutions that exceed expectations.
+	</p>
+	<p>
+		Thank you for taking the time to explore my portfolio. I invite you to review my resume and
+		browse through the projects I have worked on. If you have any questions or would like to discuss
+		potential collaborations, please feel free to reach out. I look forward to connecting with you!
+	</p>
 	<div class="resume">
-		<iframe src="https://drive.google.com/file/d/1oowKdjpnO4lX6uZSDDVosulcFw1-cGcV/preview#zoom=FitH"/>
-	</div>
-	<div>
-		<p>
-			With over 13 years of experience in software development, I have cultivated a passion for
-			programming and a deep appreciation for the art of problem-solving.
-			<br /><br />I discovered my love for programming during high school when I was introduced to
-			Java. The elegance and flexibility of the language captivated me, igniting a curiosity to
-			explore its endless possibilities. From that point on, I delved deeper into the world of
-			coding, continuously expanding my knowledge and skills.
-			<br /><br />I am always eager to take on new challenges, collaborate with talented
-			individuals, and stay up to date with the latest advancements in technology. My commitment to
-			continuous learning and my passion for programming drive me to deliver high-quality solutions
-			that exceed expectations.
-			<br /><br />Thank you for taking the time to explore my portfolio. I invite you to review my
-			resume and browse through the projects I have worked on. If you have any questions or would
-			like to discuss potential collaborations, please feel free to reach out. I look forward to
-			connecting with you!
-		</p>
+		<iframe
+			src="https://drive.google.com/file/d/1oowKdjpnO4lX6uZSDDVosulcFw1-cGcV/preview#zoom=FitH"
+		/>
 	</div>
 </div>
 
@@ -32,48 +38,79 @@
 	.aboutContainer {
 		margin-top: 75px;
 		width: 95%;
-		min-height: 800px;
 		height: fit-content;
 		min-height: 75vh;
 		display: grid;
-        gap: 1%;
+		gap: 1%;
 		grid-template-columns: 40% 60%;
 		grid-template-areas:
 			'photo resume'
-			'short resume'
-			'short resume'
-			'short resume';
-            margin-bottom: 40px;
-	}
+			'short1 resume'
+			'short2 resume'
+			'short3 resume'
+			'short4 resume';
+margin-bottom: 60px;
 
+	}
 
 	@media screen and (max-width: 1075px) {
 		.aboutContainer {
 			grid-template-columns: 35% 65%;
 			grid-template-areas:
-				'photo short'
-				'photo short'
+				'photo .'
+				'photo short1'
+				'photo short2'
+				'short3 short3'
+				'short4 short4'
 				'resume resume';
 		}
-        @media (max-width: 740px) {		
-        .aboutContainer {
-        grid-template-columns: repeat(3, 1fr);
-			grid-template-areas: '. photo .'
-				'short short short'
-				'resume resume resume';
-        row-gap: 2.5%;
 
-        }
-        .aboutContainer > div:first-of-type > img {
+		.aboutContainer > div:first-of-type {
+			align-items: center;
+		}
+	}
+	@media (max-width: 550px) {
+		.aboutContainer {
+			grid-template-columns: repeat(4, 1fr);
+			grid-template-areas:
+				'photo photo short1 short1'
+				'short2 short2 short2 short2'
+				'short3 short3 short3 short3'
+				'short4 short4 short4 short4'
+				'resume resume resume resume'
+				'resume resume resume resume';
+
+		}
+
+		.aboutContainer > div:first-of-type > img {
 			width: 80% !important;
 			height: auto;
 			align-self: center;
 			aspect-ratio: 0.75;
+			align-self: start;
 		}
-    }
-		
-		.aboutContainer > div:first-of-type {
-			align-items: center;
+		.aboutContainer > p:first-of-type {
+			width: 100%;
+			align-self: end;
+		}
+	}
+
+	@media (max-width: 635px) {
+		.aboutContainer {
+			grid-template-columns: repeat(4, 1fr);
+			grid-template-areas:
+				'photo photo short1 short1'
+				'short2 short2 short2 short2'
+				'short3 short3 short3 short3'
+				'short4 short4 short4 short4'
+				'resume resume resume resume'
+				'resume resume resume resume'
+				'. . . .';
+
+		}
+		.aboutContainer > p:first-of-type {
+			width: 95%;
+			align-self: end;
 		}
 	}
 
@@ -82,7 +119,6 @@
 		width: 100%;
 		display: flex;
 		justify-content: center;
-		
 	}
 	.resume > iframe {
 		aspect-ratio: 0.75;
@@ -99,28 +135,29 @@
 	.aboutContainer > div:first-of-type > img {
 		width: 60%;
 		height: auto;
-		aspect-ratio: .75;
+		aspect-ratio: 0.75;
 	}
-	.aboutContainer > div:last-of-type {
-		grid-area: short;
+
+	.aboutContainer > p {
 		max-width: 95%;
 		text-align: center;
 		justify-self: center;
 		font-size: 22px;
-font-family: 'Edu NSW ACT Foundation', cursive;
+		font-family: 'Edu NSW ACT Foundation', cursive;
+	}
+	.aboutContainer > p:first-of-type {
+		grid-area: short1;
+		align-self: end;
+	}
+	.aboutContainer > p:nth-of-type(2) {
+		grid-area: short2;
+		align-self: end;
+	}
+	.aboutContainer > p:nth-of-type(3) {
+		grid-area: short3;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-		
+	.aboutContainer > p:last-of-type {
+		grid-area: short4;
 	}
 </style>
